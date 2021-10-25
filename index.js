c01ce60ec87eb8a784a21d730449eead30972bb0
Merlin1A <jbrookins@umass.edu>
2021-10-25 16:47:46 -0400

    Test

--- index.js
+++ index.js
@@ -4,10 +4,28 @@ const http = require('http');
 // createServer method takes a callback function as argument
 // the callback function takes two arguments req and re
 const server = http.createServer(function (req, res) {
-    res.statusCode = 200; // 200 = OK
-    res.setHeader('Content-Type', 'text/html');
-    res.write("<h1>Demo page</h1>");
-    res.end();
+    console.log(`${req.method} request received at ${req.url}`);
+    if (req.url === '/html') {
+        res.setHeader('Content-Type', 'text/html');
+        res.statusCode = 200; // 200 = OK
+        res.write("<h1>Demo page</h1>");
+        res.end();
+    } else if (req.url === '/plain') {
+        res.setHeader('Content-Type', 'text/plain');
+        res.statusCode = 200; // 200 = OK
+        res.write("<h1>Demo page</h1>");
+        res.end();
+    } else if (req.url === '/json') {
+        res.setHeader('Content-Type', 'application/json');
+        res.statusCode = 200; // 200 = OK
+        res.write(JSON.stringify({"firstName": "Harry", "lastName": "Potter"}));
+        res.end();
+    } else {
+        res.setHeader('Content-Type', 'text/html');
+        res.statusCode = 400; // 400 = Bad request
+        res.write("<h1>Sorry, this page is not available</h1>");
+        res.end();
+    }
 });
 
 const PORT = process.env.PORT || 80;
